690700249b06a7a9a05cf2a6dd8bd6c8599a0e24
sebtedesco <stedescoc@gmail.com>
2020-01-14 17:44:49 +0000

    Completed Feature Set Two | Sebastian Tedesco

---
+++ client/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  // constructor(props) {
+  //   super(props);
+
+  // }
+
+  render() {
+    return (
+      <header>
+        <h3 className="col header pt-3 pr-3 pb-3 pl-12">$ Wicked Sales</h3>
+      </header>
+    );
+  }
+}
---
+++ client/components/product-list.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import ProductListItem from './product-list-item';
+
+export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    const init = {
+      method: 'GET'
+    };
+    fetch('/api/products/', init)
+      .then(response => {
+        return response.json();
+      })
+      .then(products => {
+        this.setState(state => ({
+          products: products
+        }));
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row justify-content-center">
+          {
+            this.state.products.map(product => {
+              return <ProductListItem
+                key={product.productId}
+                product={product}
+                // productImage={product.image}
+                // productName={product.name}
+                // productPrice={product.price}
+                // productDescription={product.shortDescription}
+              />;
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
