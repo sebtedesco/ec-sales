dd5fb3daa864496b736dbd3b09f66eedbf653594
sebtedesco <stedescoc@gmail.com>
2020-01-17 01:50:10 +0000

    parseinted price

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function CartSummaryItem(props) {
+  const price = `$${parseInt((props.cartItem.price) / 100).toFixed(2)}`;
   return (
     <div className="container border mt-2 mb-2 w-75">
       <div className="row">
@@ -10,7 +11,7 @@ export default function CartSummaryItem(props) {
           </div>
           <div className="col-8 d-inline-block pl-8">
             <h4 className="card-title">{props.cartItem.name}</h4>
-            <p className="card-text">{props.cartItem.price}</p>
+            <p className="card-text">{price}</p>
             <p className="card-text">{props.cartItem.shortDescription}</p>
           </div>
         </div>
