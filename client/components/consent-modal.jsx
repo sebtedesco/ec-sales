e685a7e9e90845ede9311cbf8c1af9c17919c3fa
sebtedesco <stedescoc@gmail.com>
2020-03-18 05:11:40 +0000

    color experimenting for theme

--- client/components/consent-modal.jsx
+++ client/components/consent-modal.jsx
@@ -5,9 +5,7 @@ export default function ConsentModal(props) {
     <div className="modal-background container position-fixed w-100 h-100">
       <div className="consent-modal">
         <h2>Important!</h2>
-        <p>This site is for educational purposes ONLY. You cannot purchase electric cars here.</p>
-        <p>This is silly nonsensical electric car shopping app to give you an idea of the cost and price comparisons of electric cars.
-        </p>
+        <p>This site is for educational purposes ONLY. You cannot purchase Eric Clapton memorabilia here.</p>
         <button type="button" className="btn btn-danger" onClick={props.hideConsentModal}>Proceed</button>
       </div>
     </div>
