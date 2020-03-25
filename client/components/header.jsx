a86145d0ab1bda1b8d4cdc5e36d1cd856a6cee55
sebtedesco <stedescoc@gmail.com>
2020-03-25 23:21:29 +0000

    finally not scrolling into header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
 
   render() {
     return (
-      <nav className="header navbar fixed-top">
+      <nav className="navbar">
         <div className="header-title" onClick={() => this.props.setViewMethod('catalog', {})}>
           Eric Clapton Souvenir Shop
         </div>
