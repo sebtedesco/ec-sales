4e6774948198516d4c1523fac576eb892e136880
sebtedesco <stedescoc@gmail.com>
2020-04-13 00:10:46 +0000

    still working on getting checkbox to work

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -27,7 +27,7 @@ export default class Checkout extends React.Component {
         creditCardNumber: false,
         expiration: false,
         cvv: false,
-        checkbox: 'init'
+        checkbox: true
       },
       errorFree: false
     };
@@ -136,23 +136,32 @@ export default class Checkout extends React.Component {
         };
         break;
       case 'checkbox':
-        if (this.state.errors.checkbox === 'init') {
-          console.log('its a string to changing to true')
-          errors.checkbox = false;
-        }else{
-          console.log(`its ${errors.checkbox} so changing to ${!errors.checkbox}`)
-          errors.checkbox = !errors.checkbox;
-        }
+        const name = event.target.name;
+        let value;
+        // const name = target.name;
+        const target = event.target;
+        const errors = {...this.state.errors}
+        errors.checkbox = target.checked;
         this.setState({
           errors
-        })
-    }
-
+        });
+      break;
+        // if (this.state.errors.checkbox === 'init') {
+        //   console.log('its a string to changing to true')
+        //   errors.checkbox = false;
+        // }else{
+        //   console.log(`its ${errors.checkbox} so changing to ${!errors.checkbox}`)
+        //   errors.checkbox = !errors.checkbox;
+        // }
+        // this.setState({
+        //   errors
+        // })
     this.setState({ errors }, () => {
       this.errorFree();
     })
     // this.setState({ errors })
     // this.errorFree();
+    }
   }
 
   errorFree() {
@@ -323,10 +332,11 @@ export default class Checkout extends React.Component {
                       type="checkbox"
                       placeholder="checkbox"
                       name="checkbox"
+                      checked={this.state.errors.checkbox}
                       className="d-inline mr-2 pb-1"
                       value={this.state.checkbox}
-                      onChange={console.log('checkbox changed')}
-                      onBlur={this.handleValidation}
+                      onChange={this.handleValidation}
+                      // onBlur={this.handleValidation}
                     /> I understand that by clicking "Place Order" I am not placing an order.</label>
                     <small className={`form-text text-muted ${this.state.errors.checkbox ? 'red' : ''}`}> {this.state.errors.checkbox ? 'You must check the box if you wish to proceed' : ''} </small>
                   </div>
