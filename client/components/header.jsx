eae1c5d2c25feca4f6d1451d819b691f30964923
sebtedesco <stedescoc@gmail.com>
2020-04-14 00:03:26 +0000

    media queries and validation work

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
     return (
       <nav className="navbar dark-background">
         <div className="header-title" onClick={() => this.props.setViewMethod('catalog', {})}>
-          Eric Clapton Souvenir Shop
+          Eric Clapton Shop
         </div>
         <button type="button" className="btn btn-light" onClick={() => this.props.setViewMethod('cart', {})}>
           <h5 className="m-0"> <i className="fas fa-shopping-cart"></i> {this.props.cartItemCount} </h5>
